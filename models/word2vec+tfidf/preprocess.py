215dd5cb5b035bdbed2f58c4710dd10d0975ae8c
lindayong <164532640@qq.com>
2016-12-05 21:52:24 +0800

    For change log, please check word2vec+TFIDF.md

--- models/word2vec+tfidf/preprocess.py
+++ models/word2vec+tfidf/preprocess.py
@@ -2,24 +2,30 @@
 # coding=utf-8
 
 # 输入数据文件的路径和名字
-# 输出处理后的数据
+# 输出处理后的文件
+
 
 import re
 
-
 def preprocess(file):
-    num = 0
-    with open(file) as f:
-        for line in f:
-            num += 1
-            matchObj = re.match('"lyrics":"(.*?)"}', line, re.M|re.I)
-            if matchObj:
-                print("matchObj.group(2): ", matchObj.group(2))
-            print(line)
-            if num > 100:
-                break
+    write_file = file + '_processed'
+    temp_num = 0
+    with open(file) as f_read:
+        with open(write_file, 'w') as f_write:
+            for line in f_read:
+                # 匹配歌词字段
+                matchObj = re.match(r'.*"lyrics":"(.*?)"}', line, re.M|re.I)
 
+                if matchObj and matchObj.group(1):
+                    # 替换非中文字符为空格
+                    song = re.sub(u'[^\u4e00-\u9fa5]', u' ', matchObj.group(1))
+                    song = re.sub('\s+', ' ', song)
+                    if len(re.sub(u' ', u'', song)) > 30:
+                        f_write.write(song + '\n')
 
-file = "lyrics.json"
+                temp_num += 1
+                print('+++ Processing song', temp_num)
 
-preprocess(file)
+if __name__ == '__main__':
+    file = 'lyrics.json'
+    preprocess(file)
