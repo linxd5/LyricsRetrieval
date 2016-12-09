cd46434598e1ed4fd6ba14ce78669075dc35342a
lindayong <164532640@qq.com>
2016-12-09 16:05:20 +0800

    For change log, please check word2vec+TFIDF.md

--- models/word2vec+tfidf/web/static/index.js
+++ models/word2vec+tfidf/web/static/index.js
@@ -1,28 +1,36 @@
 // Apache server ip config
 
-$(function(){
-	$('#query_button').click(function(){
+$(function () {
+    $('#query_button').click(query);
 
-		$.ajax({
-			url: '/query',
-			data: $('form').serialize(),
-			type: 'POST',
-			success: function(response){
-				$('#lyrics_container').remove();
-			    sim_lyrics = '<div id="lyrics_container">';
-				for (i=0; i<response['sim_lyrics'].length; i++) {
-					id_html = '<div><a href="' + 'http://music.163.com/#/song?id=' + response['sim_lyrics'][i][0]
-						+ '">' + response['sim_lyrics'][i][0] +'</a></div>';
-					console.log(id_html);
+    $('input').keypress(function (event) {
+        if (event.which == 13) {
+            event.preventDefault();
+            query();
+        }
+    });
+
+    function query() {
+        $.ajax({
+            url: '/query',
+            data: $('form').serialize(),
+            type: 'POST',
+            success: function (response) {
+                $('#lyrics_container').remove();
+                sim_lyrics = '<div id="lyrics_container">';
+                for (i = 0; i < response['sim_lyrics'].length; i++) {
+                    id_html = '<div><a href="' + 'http://music.163.com/#/song?id=' + response['sim_lyrics'][i][0]
+                        + '">' + response['sim_lyrics'][i][0] + '</a></div>';
+                    console.log(id_html);
                     lyric_html = '<div>' + response['sim_lyrics'][i][1] + '</div>';
                     sim_lyrics += '<div id="lyric_container">' + id_html + lyric_html + '</div>' + '<br /><br />';
-				}
-				sim_lyrics += '</div>';
+                }
+                sim_lyrics += '</div>';
                 $('.jumbotron').append(sim_lyrics);
-			},
-			error: function(error){
-				console.log(error);
-			}
-		});
-	});
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    }
 });
